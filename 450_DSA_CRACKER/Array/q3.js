4a30f9e712b8512dad0477c8466e21f360822b67
madhurendra1311 <kmadhu1311@gmail.com>
2022-08-07 22:58:51 +0530

    kth smallest array

--- 450_DSA_CRACKER/Array/q3.js
+++ 450_DSA_CRACKER/Array/q3.js
@@ -4,6 +4,19 @@
 
 // Approach 1:
 
-const kthSmallest = (arr,l,r,k) => {
-    //code here
+const kthSmallest = (arr, k) => {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            if(arr[i] > arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr[arr.length - k];
 }
+
+let arr = [1, 2, 5, 6, 4]
+let res = kthSmallest(arr, 3)
+console.log(res);
