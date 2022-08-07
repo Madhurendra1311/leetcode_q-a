57c62dcbf3e2d42804a1ba082f48f395b24b9334
madhurendra1311 <kmadhu1311@gmail.com>
2022-08-07 20:33:10 +0530

    kth smallest array

---
+++ 450_DSA_CRACKER/Array/q3.js
@@ -0,0 +1,9 @@
+// link: https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1
+
+// Kth smallest element
+
+// Approach 1:
+
+const kthSmallest = (arr,l,r,k) => {
+    //code here
+}
