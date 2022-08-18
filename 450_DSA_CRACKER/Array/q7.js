ee19c1d81b0cbfadea752b4b55cc8dc92c869805
madhurendra1311 <kmadhu1311@gmail.com>
2022-08-18 12:56:09 +0530

    Cyclically rotate an array by one

--- 450_DSA_CRACKER/Array/q7.js
+++ 450_DSA_CRACKER/Array/q7.js
@@ -13,10 +13,37 @@ const rotate = (arr) => {
         arr[i] = arr[i-1];
     }
     arr[0] = x;
-    
+
     return arr;
 }
 
 let arr = [1, 2, 3, 4, 5]
 let res = rotate(arr);
 console.log(res);
+
+// Approach 2:
+// Two pointers
+
+// say i and j which point to first and last element of array respectively.
+// As we know in cyclic rotation we will bring last element to first and shift rest in forward direction,
+// so start swaping arr[i] and arr[j] and keep j fixed and i moving towards j.
+// Repeat till i is not equal to j
+
+
+const rotateArray = (arr) => {
+    let i = 0;
+    let j = arr.length-1;
+    while(i != j){
+        let temp;
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j]= temp;
+        i++;
+    }
+    return arr;
+}
+
+let array = [1, 2, 3, 4, 5]
+let result = rotate(array);
+console.log(result);
+
