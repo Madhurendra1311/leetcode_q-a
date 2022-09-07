7108736f5a8f4dd064d806d00499e5e72a63c1e3
madhurendra1311 <kmadhu1311@gmail.com>
2022-09-08 01:01:38 +0530

    subWithZero

--- 450_DSA_CRACKER/Array/q20.js
+++ 450_DSA_CRACKER/Array/q20.js
@@ -2,7 +2,7 @@
 
 // link: https://practice.geeksforgeeks.org/problems/subarray-with-0-sum-1587115621/1
 
-let subWithZero = (arr) => {
+const subWithZero = (arr) => {
     //loop through the array
     for(let i = 0; i < arr.length; i++){
       let sum = arr[i];
@@ -25,3 +25,34 @@ let subWithZero = (arr) => {
   }
 
 console.log(subWithZero([1, 2, 3, 4, 5]));
+
+const subWithZeros = (arr) => {
+    //create a new set
+    let set = new Set();
+   
+    //add 0 to handle the case when first  element in array is 0 
+    set.add(0);
+    
+     //To calculate the sum
+     let sum = 0;
+   
+     //loop through the array
+     for(let i = 0; i < arr.length; i++){
+   
+       //calculate the sum of the subarrays
+       sum += arr[i];
+       
+       //if sum is already there, then subarray with 0 is found
+       if(set.has(sum)){
+          return true;
+       }
+       
+       //Add sum to the set
+       set.add(sum);
+     }
+     
+     //Return false by default
+     return false;
+   }
+
+   console.log(subWithZeros([1, 2, 3, 4, 5]));
