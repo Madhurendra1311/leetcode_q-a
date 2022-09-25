7f8fca9bf0b40c42216a362367d0f9daf1519212
madhurendra1311 <kmadhu1311@gmail.com>
2022-09-25 10:08:06 +0530

    check_rotation_string

--- 450_DSA_CRACKER/String/q4.js
+++ 450_DSA_CRACKER/String/q4.js
@@ -12,3 +12,36 @@ const checkString = (s1, s2) => {
 
 let res = checkString('ABCD', 'CDAB')
 console.log(res);
+
+
+const check_rotation = (s, goal) => {
+    if (s.length != goal.length){
+        return false;
+    }
+    let q1 = []
+    for(let i=0;i<s.length;i++) {
+        q1.push(s[i])
+    }
+ 
+    let q2 = []
+    for(let i=0;i<goal.length;i++) {
+        q2.push(goal[i])
+    }
+ 
+    let k = goal.length
+    while (k--){
+        let ch = q2[0]
+        console.log(ch, '34');
+        q2.shift()
+        console.log(q2, '36');
+        q2.push(ch)
+        console.log(q2, '38');
+        if (JSON.stringify(q2) == JSON.stringify(q1)) {
+            return true
+        }
+    }
+    return false
+}
+
+let res1 = check_rotation("ABCD", "CDAB")
+console.log(res1);
